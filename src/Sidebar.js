64b82d8dbc4391c1636db460f6c16835cc6e5840
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-06-26 18:16:06 -0500

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+const Sidebar = () => {
+  return (
+    <div className = "sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+      {/* SidebarOption */}
+
+      {/* SidebarOption */}
+
+      {/* SidebarOption */}
+
+      {/* SidebarOption */}
+
+      {/* SidebarOption */}
+
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
